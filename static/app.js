138a42d1a9672bf03d3850a13f129e695a5f28b9
Kevin Casey <kacasey@berkeley.edu>
2015-10-10 15:26:18 -0700

    added crowns and checkboxes

--- static/app.js
+++ static/app.js
@@ -109,7 +109,7 @@ function setText(element, text) {
 function updateStateUi(state) {
   var currentState = state['state'];
   if (currentState == 'Not Started') {
-    $('#game_setup_image').show();
+    $('#game_setup_top').show();
     $('#game_start').show();
     $('#game_information').hide();
     $('#game_board').hide();
@@ -121,7 +121,7 @@ function updateStateUi(state) {
       $('#start_game_button').hide();
     }
   } else {
-    $('#game_setup_image').hide();
+    $('#game_setup_top').hide();
     $('#game_start').hide();
     $('#game_information').show();
     $('#game_board').show();
@@ -195,5 +195,5 @@ function init() {
 gadgets.util.registerOnLoadHandler(init);
 
 $(document).ready(function() {
-  $('start_game_button').click(assignRoles);
+  $('#start_game_button').click(assignRoles);
 })
